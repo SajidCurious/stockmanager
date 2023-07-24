30f7af97bea0facedf8f79262b5cda7e077e9ed6
Mohammed <sajidcurious9@gmail.com>
2023-07-25 01:53:57 +0530

    added authorization using the mongodb connect link

--- src/app/api/mongo/route.js
+++ src/app/api/mongo/route.js
@@ -1,23 +1,28 @@
 import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
 
-// Replace the uri string with your connection string.
-const uri = "<connection string uri>";
+export async function GET(request) {
+  // Replace the uri string with your connection string.
+  const uri =
+    "mongodb+srv://sajidcurious9:he5ookTWGxxga0HW@cluster0.diwq20h.mongodb.net/";
 
-const client = new MongoClient(uri);
+  const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    const database = client.db("sample_mflix");
-    const movies = database.collection("movies");
+  async function run() {
+    try {
+      const database = client.db("sajid");
+      const movies = database.collection("inventory");
 
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: "Back to the Future" };
-    const movie = await movies.findOne(query);
+      // Query for a movie that has the title 'Back to the Future'
+      const query = {};
+      const movie = await movies.findOne(query);
 
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+      console.log(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
   }
+  run().catch(console.dir);
+  return NextResponse.json({ a: 34 });
 }
-run().catch(console.dir);
