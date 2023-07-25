71f7705702ea6f19de25cd5fb5635c6e6ff063d3
Mohammed <sajidcurious9@gmail.com>
2023-07-26 00:48:43 +0530

    added post request api code

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -23,3 +23,26 @@ export async function GET(request) {
     await client.close();
   }
 }
+
+export async function POST(request) {
+  // Replace the uri string with your connection string.
+
+  let body = request.body;
+  const uri =
+    "mongodb+srv://sajidcurious9:he5ookTWGxxga0HW@cluster0.diwq20h.mongodb.net/";
+
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("sajidStocks");
+    const inventory = database.collection("inventory");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const product = await inventory.insertOne(body);
+
+    return NextResponse.json({ product });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
