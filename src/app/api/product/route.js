7d0c96637514f69314f4291fb230a844b7208e50
Mohammed <sajidcurious9@gmail.com>
2023-07-26 02:02:04 +0530

    edited

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -27,7 +27,7 @@ export async function GET(request) {
 export async function POST(request) {
   // Replace the uri string with your connection string.
 
-  let body = request.body;
+  let body = await request.json();
   const uri =
     "mongodb+srv://sajidcurious9:he5ookTWGxxga0HW@cluster0.diwq20h.mongodb.net/";
 
@@ -40,7 +40,7 @@ export async function POST(request) {
     // Query for a movie that has the title 'Back to the Future'
     const product = await inventory.insertOne(body);
 
-    return NextResponse.json({ product });
+    return NextResponse.json({ product, ok: true });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
