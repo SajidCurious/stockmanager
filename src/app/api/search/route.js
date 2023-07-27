d04199453bf0184cc354ba80d66ceb7c35ff4bb4
Mohammed <sajidcurious9@gmail.com>
2023-07-28 01:26:40 +0530

    added the code to search in the paltform and it will get the relevant searched data from mongodb

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -2,6 +2,7 @@ import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
+  const query = request.nextUrl.searchParams.get("query");
   // Replace the uri string with your connection string.
   const uri =
     "mongodb+srv://sajidcurious9:he5ookTWGxxga0HW@cluster0.diwq20h.mongodb.net/";
@@ -12,12 +13,20 @@ export async function GET(request) {
     const database = client.db("sajidStocks");
     const inventory = database.collection("inventory");
 
-    // Query for a movie that has the title 'Back to the Future'
-    const query = {};
-    const allProducts = await inventory.find(query).toArray();
+    const products = await inventory
+      .aggregate([
+        {
+          $match: {
+            $or: [
+              { slug: { $regex: query, $options: "i" } }, // Partial matching for name field
+            ],
+          },
+        },
+      ])
+      .toArray();
 
     // console.log(movie);
-    return NextResponse.json({ allProducts });
+    return NextResponse.json({ success: true, products });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
