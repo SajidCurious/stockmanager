276d3d4a0404e16eaadd9b2a4012992e0bed9908
Mohammed <sajidcurious9@gmail.com>
2023-07-28 00:50:21 +0530

    added a folder inside api to write a code if someone searches something the relevant data should appear which is stored in mongodb

---
+++ src/app/api/search/route.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  // Replace the uri string with your connection string.
+  const uri =
+    "mongodb+srv://sajidcurious9:he5ookTWGxxga0HW@cluster0.diwq20h.mongodb.net/";
+
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("sajidStocks");
+    const inventory = database.collection("inventory");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = {};
+    const allProducts = await inventory.find(query).toArray();
+
+    // console.log(movie);
+    return NextResponse.json({ allProducts });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
