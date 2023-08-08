d8d057c7f4459adfb67d724f9c67ff75c67433b3
Mohammed <sajidcurious9@gmail.com>
2023-08-08 23:43:41 +0530

    removed li from nav because i'm not using them

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -19,12 +19,6 @@ const Header = () => {
           </svg>
           <span className="ml-3 text-xl">StockManager</span>
         </a>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-        </nav>
       </div>
     </header>
   );
